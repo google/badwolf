f0559a27eba85eaef63332419564618d4ad39802
xllora <xllora@google.com>
2015-10-09 22:26:44 -0700

    Add basic result functional table

--- bql/table/table.go
+++ bql/table/table.go
@@ -1,12 +1,35 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package table export the table that contains the results of a BQL query.
 package table
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	"github.com/google/badwolf/triple/literal"
+	"github.com/google/badwolf/triple/node"
+	"github.com/google/badwolf/triple/predicate"
+)
 
 // Table contains the results of a BQL query.
 type Table struct {
-	bs  []string
-	mbs map[string]bool
+	bs   []string
+	mbs  map[string]bool
+	data []Row
 }
 
 // New returns a new table that can hold data for the the given bindings. The,
@@ -23,4 +46,112 @@ func New(bs []string) (*Table, error) {
 		bs:  bs,
 		mbs: m,
 	}, nil
+}
+
+// Cell contains one of the possible values that form rows.
+type Cell struct {
+	S string
+	N *node.Node
+	P *predicate.Predicate
+	L *literal.Literal
+	T *time.Time
+}
+
+// String returns a readable representation of a cell.
+func (c *Cell) String() string {
+	if c.S != "" {
+		return c.S
+	}
+	if c.N != nil {
+		return c.N.String()
+	}
+	if c.P != nil {
+		return c.P.String()
+	}
+	if c.L != nil {
+		return c.L.String()
+	}
+	if c.T != nil {
+		return c.T.Format(time.RFC3339Nano)
+	}
+	return "<NULL>"
+}
+
+// Row represents a collection of cells.
+type Row map[string]*Cell
+
+// ToTextLine converts a row into line of text. To do so, it requires the list
+// of bindings of the table, and the separator you want to use. If the separator
+// is empty tabs will be used.
+func (r Row) ToTextLine(res *bytes.Buffer, bs []string, sep string) error {
+	cnt := len(bs)
+	if sep == "" {
+		sep = "\t"
+	}
+	for _, b := range bs {
+		cnt--
+		v := "<NULL>"
+		if c, ok := r[b]; ok {
+			v = c.String()
+		}
+		if _, err := res.WriteString(v); err != nil {
+			return err
+		}
+		if cnt > 0 {
+			res.WriteString(sep)
+		}
+	}
+	return nil
+}
+
+// AddRow adds a row to the end of a table. For preformance reasons, it does not
+// check that all bindindgs are set, nor that they are declared on table
+// creation. BQL builds valid tables, if you plan to create tables on your own
+// you should be carful to provide valid rows.
+func (t *Table) AddRow(r Row) {
+	t.data = append(t.data, r)
+}
+
+// NumRows returns the number of rows currently available on the table.
+func (t *Table) NumRows() int {
+	return len(t.data)
+}
+
+// Row returns the requested row. Rows start at 0. Also, if you request a row
+// beyond it will return nil, and the ok boolean will be false.
+func (t *Table) Row(i int) (Row, bool) {
+	if i >= len(t.data) {
+		return nil, false
+	}
+	return t.data[i], true
+}
+
+// Rows returns all the available rows.
+func (t *Table) Rows() []Row {
+	return t.data
+}
+
+// Bindings returns the bindings contained on the tables.
+func (t *Table) Bindings() []string {
+	return t.bs
+}
+
+// ToText convert the table into a readable text versions. It requires the
+// separator to be used between cells.
+func (t *Table) ToText(sep string) (*bytes.Buffer, error) {
+	res, row := &bytes.Buffer{}, &bytes.Buffer{}
+	for _, r := range t.data {
+		err := r.ToTextLine(row, t.bs, sep)
+		if err != nil {
+			return nil, err
+		}
+		if _, err := res.Write(row.Bytes()); err != nil {
+			return nil, err
+		}
+		if _, err := res.WriteString("\n"); err != nil {
+			return nil, err
+		}
+		row.Reset()
+	}
+	return res, nil
 }
--- bql/table/table_test.go
+++ bql/table/table_test.go
@@ -1,6 +1,30 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package table
 
-import "testing"
+import (
+	"bytes"
+	"errors"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/google/badwolf/triple/literal"
+	"github.com/google/badwolf/triple/node"
+	"github.com/google/badwolf/triple/predicate"
+)
 
 func TestNew(t *testing.T) {
 	testTable := []struct {
@@ -17,5 +41,99 @@ func TestNew(t *testing.T) {
 		if _, err := New(entry.bs); (err == nil) == entry.err {
 			t.Errorf("table.Name failed; want %v for %v ", entry.err, entry.bs)
 		}
+	}
+}
+
+func TestCellString(t *testing.T) {
+	now := time.Now()
+	n := node.NewBlankNode()
+	p, err := predicate.NewImmutable("foo")
+	if err != nil {
+		t.Fatalf("failed to create predicate with error %v", err)
+	}
+	l, err := literal.DefaultBuilder().Parse(`"true"^^type:bool`)
+	if err != nil {
+		t.Fatalf("failed to create literal with error %v", err)
+	}
+	testTable := []struct {
+		c    *Cell
+		want string
+	}{
+		{c: &Cell{S: "foo"}, want: `foo`},
+		{c: &Cell{N: n}, want: n.String()},
+		{c: &Cell{P: p}, want: p.String()},
+		{c: &Cell{L: l}, want: l.String()},
+		{c: &Cell{T: &now}, want: now.Format(time.RFC3339Nano)},
+	}
+	for _, entry := range testTable {
+		if got := entry.c.String(); got != entry.want {
+			t.Errorf("Cell.String failed to return the right string; got %q, want %q", got, entry.want)
+		}
+	}
+}
+
+func TestRowToTextLine(t *testing.T) {
+	r, b := make(Row), &bytes.Buffer{}
+	r["?foo"] = &Cell{S: "foo"}
+	r["?bar"] = &Cell{S: "bar"}
+	err := r.ToTextLine(b, []string{"?foo", "?bar"}, "")
+	if err != nil {
+		t.Errorf("row.ToTextLine failed to serialize the row with error %v", err)
+	}
+	if got, want := b.String(), "foo\tbar"; got != want {
+		t.Errorf("row.ToTextLine failed to serialize the row; got %q, want %q", got, want)
+	}
+}
+
+func TestTableManipulation(t *testing.T) {
+	newRow := func() Row {
+		r := make(Row)
+		r["?foo"] = &Cell{S: "foo"}
+		r["?bar"] = &Cell{S: "bar"}
+		return r
+	}
+	tbl, err := New([]string{"?foo", "?bar"})
+	if err != nil {
+		t.Fatal(errors.New("tbl.New failed to crate a new valid table"))
+	}
+	for i := 0; i < 10; i++ {
+		tbl.AddRow(newRow())
+	}
+	if got, want := tbl.NumRows(), 10; got != want {
+		t.Errorf("tbl.Number: got %d,  wanted %d instead", got, want)
+	}
+	c := newRow()
+	for _, r := range tbl.Rows() {
+		if !reflect.DeepEqual(c, r) {
+			t.Errorf("tbl contains inconsitent row %v, want %v", r, c)
+		}
+	}
+	for i := 0; i < 10; i++ {
+		if r, ok := tbl.Row(i); !ok || !reflect.DeepEqual(c, r) {
+			t.Errorf("tbl contains inconsitent row %v, want %v", r, c)
+		}
+	}
+	if got, want := tbl.Bindings(), []string{"?foo", "?bar"}; !reflect.DeepEqual(got, want) {
+		t.Errorf("tbl.Bindings() return inconsistent bindings; got %v, want %v", got, want)
+	}
+}
+
+func TestTableToText(t *testing.T) {
+	newRow := func() Row {
+		r := make(Row)
+		r["?foo"] = &Cell{S: "foo"}
+		r["?bar"] = &Cell{S: "bar"}
+		return r
+	}
+	tbl, err := New([]string{"?foo", "?bar"})
+	if err != nil {
+		t.Fatal(errors.New("tbl.New failed to crate a new valid table"))
+	}
+	for i := 0; i < 3; i++ {
+		tbl.AddRow(newRow())
+	}
+	want := "foo, bar\nfoo, bar\nfoo, bar\n"
+	if got, err := tbl.ToText(", "); err != nil || got.String() != want {
+		t.Errorf("tbl.ToText failed to rerialize the text;\nGot:\n%s\nWant:\n%s", got, want)
 	}
 }
