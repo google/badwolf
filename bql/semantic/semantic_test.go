a5d74ad6ad769616f6f224a5328491f2646433eb
xllora <xllora@google.com>
2015-10-25 19:06:38 -0700

    Fix accidental circular dependency introduced on semantic tests.

--- bql/semantic/semantic_test.go
+++ bql/semantic/semantic_test.go
@@ -18,8 +18,6 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/google/badwolf/bql/grammar"
-	"github.com/google/badwolf/bql/semantic"
 	"github.com/google/badwolf/triple"
 	"github.com/google/badwolf/triple/literal"
 	"github.com/google/badwolf/triple/node"
@@ -85,111 +83,6 @@ func TestGraphClauseManipulation(t *testing.T) {
 	st.AddWorkingGrpahClause()
 	if got, want := len(st.GraphPatternClauses()), 1; got != want {
 		t.Fatalf("semantic.GraphClause.Clauses return wrong number of clauses in %v; got %d, want %d", st, got, want)
-	}
-}
-
-func TestAcceptOpsByParseAndSemantic(t *testing.T) {
-	table := []struct {
-		query   string
-		graphs  int
-		triples int
-	}{
-		// Insert data.
-		{`insert data into ?a {/_<foo> "bar"@[1975-01-01T00:01:01.999999999Z] /_<foo>};`, 1, 1},
-		{`insert data into ?a {/_<foo> "bar"@[] "bar"@[1975-01-01T00:01:01.999999999Z]};`, 1, 1},
-		{`insert data into ?a {/_<foo> "bar"@[] "yeah"^^type:text};`, 1, 1},
-		// Insert into multiple graphs.
-		{`insert data into ?a,?b,?c {/_<foo> "bar"@[] /_<foo>};`, 3, 1},
-		// Insert multiple data.
-		{`insert data into ?a {/_<foo> "bar"@[] /_<foo> .
-			                      /_<foo> "bar"@[] "bar"@[1975-01-01T00:01:01.999999999Z] .
-			                      /_<foo> "bar"@[] "yeah"^^type:text};`, 1, 3},
-		// Delete data.
-		{`delete data from ?a {/_<foo> "bar"@[] /_<foo>};`, 1, 1},
-		{`delete data from ?a {/_<foo> "bar"@[] "bar"@[1975-01-01T00:01:01.999999999Z]};`, 1, 1},
-		{`delete data from ?a {/_<foo> "bar"@[] "yeah"^^type:text};`, 1, 1},
-		// Delete from multiple graphs.
-		{`delete data from ?a,?b,?c {/_<foo> "bar"@[1975-01-01T00:01:01.999999999Z] /_<foo>};`, 3, 1},
-		// Delete multiple data.
-		{`delete data from ?a {/_<foo> "bar"@[] /_<foo> .
-			                      /_<foo> "bar"@[] "bar"@[1975-01-01T00:01:01.999999999Z] .
-			                      /_<foo> "bar"@[] "yeah"^^type:text};`, 1, 3},
-		// Create graphs.
-		{`create graph ?foo;`, 1, 0},
-		// Drop graphs.
-		{`drop graph ?foo, ?bar;`, 2, 0},
-	}
-	p, err := grammar.NewParser(grammar.SemanticBQL())
-	if err != nil {
-		t.Errorf("grammar.NewParser: should have produced a valid BQL parser")
-	}
-	for _, entry := range table {
-		st := &semantic.Statement{}
-		if err := p.Parse(grammar.NewLLk(entry.query, 1), st); err != nil {
-			t.Errorf("Parser.consume: failed to accept entry %q with error %v", entry, err)
-		}
-		if got, want := len(st.Graphs()), entry.graphs; got != want {
-			t.Errorf("Parser.consume: failed to collect right number of graphs for case %v; got %d, want %d", entry, got, want)
-		}
-		if got, want := len(st.Data()), entry.triples; got != want {
-			t.Errorf("Parser.consume: failed to collect right number of triples for case %v; got %d, want %d", entry, got, want)
-		}
-	}
-}
-
-func TestAcceptQueryBySemanticParse(t *testing.T) {
-	table := []string{
-		// Test well type litterals are accepted.
-		`select ?s from ?g where{?s ?p "1"^^type:int64};`,
-		// Test predicates are accepted.
-		// Test invalid predicate time anchor are rejected.
-		`select ?s from ?b where{/_<foo> as ?s "id"@[2015] ?o};`,
-		`select ?s from ?b where{/_<foo> as ?s "id"@[2015-07] ?o};`,
-		`select ?s from ?b where{/_<foo> as ?s "id"@[2015-07-19] ?o};`,
-		`select ?s from ?g where{/_<foo> as ?s "id"@[2015-07-19T13:12] ?o};`,
-		`select ?s from ?g where{/_<foo> as ?s "id"@[2015-07-19T13:12:04] ?o};`,
-		`select ?s from ?g where{/_<foo> as ?s "id"@[2015-07-19T13:12:04.669618843] ?o};`,
-		`select ?s from ?g where{/_<foo> as ?s "id"@[2015-07-19T13:12:04.669618843-07:00] ?o};`,
-		`select ?s from ?g where{/_<foo> as ?s "id"@[2015-07-19T13:12:04.669618843-07:00] as ?p ?o};`,
-		`select ?s from ?g where{/_<foo> as ?s  ?p "id"@[2015-07-19T13:12:04.669618843-07:00] as ?o};`,
-		// Test predicates with bindings are accepted.
-		`select ?s from ?g where{/_<foo> as ?s "id"@[?ta] ?o};`,
-		`select ?s from ?g where{/_<foo> as ?s  ?p "id"@[?ta] as ?o};`,
-		// Test predicate bounds are accepted.
-		`select ?s from ?g where{/_<foo> as ?s "id"@[2015-07-19T13:12:04.669618843-07:00, 2016-07-19T13:12:04.669618843-07:00] ?o};`,
-		`select ?s from ?g where{/_<foo> as ?s  ?p "id"@[2015-07-19T13:12:04.669618843-07:00, 2016-07-19T13:12:04.669618843-07:00] as ?o};`,
-		// Test predicate bounds with bounds are accepted.
-		`select ?s from ?g where{/_<foo> as ?s "id"@[?foo, 2016-07-19T13:12:04.669618843-07:00] ?o};`,
-		`select ?s from ?g where{/_<foo> as ?s  ?p "id"@[2015-07-19T13:12:04.669618843-07:00, ?bar] as ?o};`,
-		`select ?s from ?g where{/_<foo> as ?s  ?p "id"@[?foo, ?bar] as ?o};`}
-	p, err := grammar.NewParser(grammar.SemanticBQL())
-	if err != nil {
-		t.Errorf("grammar.NewParser: should have produced a valid BQL parser")
-	}
-	for _, input := range table {
-		if err := p.Parse(grammar.NewLLk(input, 1), &semantic.Statement{}); err != nil {
-			t.Errorf("Parser.consume: failed to accept input %q with error %v", input, err)
-		}
-	}
-}
-
-func TestRejectByParseAndSemantic(t *testing.T) {
-	table := []string{
-		// Test wront type litterals are rejected.
-		`select ?s from ?g where{?s ?p "true"^^type:int64};`,
-		// Test invalid predicate bounds are rejected.
-		`select ?s from ?b where{/_<foo> as ?s "id"@[2018-07-19T13:12:04.669618843-07:00, 2015-07-19T13:12:04.669618843-07:00] ?o};`,
-		`select ?s from ?b where{/_<foo> as ?s  ?p "id"@[2019-07-19T13:12:04.669618843-07:00, 2015-07-19T13:12:04.669618843-07:00] as ?o};`,
-	}
-	p, err := grammar.NewParser(grammar.SemanticBQL())
-	if err != nil {
-		t.Errorf("grammar.NewParser: should have produced a valid BQL parser")
-	}
-	for _, entry := range table {
-		st := &semantic.Statement{}
-		if err := p.Parse(grammar.NewLLk(entry, 1), st); err == nil {
-			t.Errorf("Parser.consume: failed to reject invalid semantic entry %q", entry)
-		}
 	}
 }
 
