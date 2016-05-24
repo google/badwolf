09f9a7aed5a9b1086e55438ae648a1ed0d862f2b
Xavier Llorà <xllora@google.com>
2016-05-24 08:16:52 -0700

    Promote to 4. stable

--- bql/version/version.go
+++ bql/version/version.go
@@ -20,7 +20,7 @@ var (
 	// Minor is the current minor version of master branch.
 	Minor = 4
 	// Patch is the curernt patched version of the master branch.
-	Patch = 3
+	Patch = 2
 	// Release is the current release level of the master branch. Valid values
 	// are dev (developement unreleased), rcX (release candidate with current
 	// iteration), stable (indicates a final released version).
