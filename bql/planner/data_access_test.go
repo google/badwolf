7fe1b4dfdb75554869a964d14c395c7e42ec58ce
Roger Leite Lucena <rogerleitelucena@gmail.com>
2020-08-06 11:12:26 -0300

    Add tests for AT and time anchor bindings for immutable predicates in "data_access_test.go"

--- bql/planner/data_access_test.go
+++ bql/planner/data_access_test.go
@@ -409,11 +409,12 @@ func TestDataAccessTripleToRowSubjectBindings(t *testing.T) {
 }
 
 func TestDataAccessTripleToRowPredicateBindings(t *testing.T) {
-	n, p, _ := testNodeTemporalPredicateLiteral(t)
-	ta, err := p.TimeAnchor()
+	n, pTemporal, _ := testNodeTemporalPredicateLiteral(t)
+	ta, err := pTemporal.TimeAnchor()
 	if err != nil {
 		t.Fatal(err)
 	}
+	_, pImmutable, _ := testNodePredicateLiteral(t)
 
 	testTable := []struct {
 		t              string
@@ -425,7 +426,7 @@ func TestDataAccessTripleToRowPredicateBindings(t *testing.T) {
 		pAnchorAlias   *table.Cell
 	}{
 		{
-			t: fmt.Sprintf("%s\t%s\t%s", n, p, n),
+			t: fmt.Sprintf("%s\t%s\t%s", n, pTemporal, n),
 			cls: &semantic.GraphClause{
 				PBinding:       "?p",
 				PAlias:         "?alias",
@@ -433,11 +434,26 @@ func TestDataAccessTripleToRowPredicateBindings(t *testing.T) {
 				PAnchorBinding: "?anchorBinding",
 				PAnchorAlias:   "?anchorAlias",
 			},
-			pBinding:       &table.Cell{P: p},
-			pAlias:         &table.Cell{P: p},
-			pIDAlias:       &table.Cell{S: table.CellString(string(p.ID()))},
+			pBinding:       &table.Cell{P: pTemporal},
+			pAlias:         &table.Cell{P: pTemporal},
+			pIDAlias:       &table.Cell{S: table.CellString(string(pTemporal.ID()))},
 			pAnchorBinding: &table.Cell{T: ta},
 			pAnchorAlias:   &table.Cell{T: ta},
+		},
+		{
+			t: fmt.Sprintf("%s\t%s\t%s", n, pImmutable, n),
+			cls: &semantic.GraphClause{
+				PBinding:       "?p",
+				PAlias:         "?alias",
+				PIDAlias:       "?id",
+				PAnchorBinding: "?anchorBinding",
+				PAnchorAlias:   "?anchorAlias",
+			},
+			pBinding:       &table.Cell{P: pImmutable},
+			pAlias:         &table.Cell{P: pImmutable},
+			pIDAlias:       &table.Cell{S: table.CellString(string(pImmutable.ID()))},
+			pAnchorBinding: &table.Cell{},
+			pAnchorAlias:   &table.Cell{},
 		},
 	}
 
