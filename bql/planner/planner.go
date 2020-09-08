eb5a1ec412fd5e6634a882515357addd919dd813
Roger Leite Lucena <rogerleitelucena@gmail.com>
2020-09-08 18:32:32 -0300

    Add "filters" to the "queryPlan" type

--- bql/planner/planner.go
+++ bql/planner/planner.go
@@ -267,6 +267,7 @@ type queryPlan struct {
 	grfsNames []string
 	grfs      []storage.Graph
 	cls       []*semantic.GraphClause
+	filters   []*semantic.FilterClause
 	tbl       *table.Table
 	chanSize  int
 	tracer    io.Writer
@@ -293,6 +294,7 @@ func newQueryPlan(ctx context.Context, store storage.Store, stm *semantic.Statem
 		bndgs:     bs,
 		grfsNames: stm.InputGraphNames(),
 		cls:       stm.GraphPatternClauses(),
+		filters:   stm.FilterClauses(),
 		tbl:       t,
 		chanSize:  chanSize,
 		tracer:    w,
